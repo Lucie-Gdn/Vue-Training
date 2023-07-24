680a2964ee9398320248db8430ec0462b7fc87c1
Lucie.Gendron <lucie.gendron.brun@gmail.com>
2023-07-24 15:38:04 +0200

    display image - bind an attribute and expression - url of obejct to href

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Socks'
+            product: 'Socks',
+            description : "great for winter",
+            image : './assets/images/socks_green.jpg',
+            url : 'https://dribbble.com/tags/distorted%20type'
         }
     }
 })
