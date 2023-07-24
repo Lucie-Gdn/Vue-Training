82dcb75fdbc7cec9e40a009239fc1977c8f23f86
Lucie.Gendron <lucie.gendron.brun@gmail.com>
2023-07-24 15:27:49 +0200

    start project - display product name and description

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = 'Socks'
+const app =Vue.createApp({ // have to pass createApp at least one object even an empty one
+    data(){ // shortcut: data:function()=data()
+        return{
+            product:'Socks',
+            description:'great for winter'
+        }
+    }
+})
