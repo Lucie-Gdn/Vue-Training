b0081b9e5dd328fa63114dd258d449a6b85d1bad
Lucie.Gendron <lucie.gendron.brun@gmail.com>
2023-07-24 15:55:23 +0200

    add condition display with inventory and sales

--- main.js
+++ main.js
@@ -2,7 +2,10 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
-            image: './assets/images/socks_blue.jpg'
+            image: './assets/images/socks_blue.jpg',
+            // inStock : true (--> on passe à un iventaire integer pour plus de précision)
+            inventory : 10,
+            onSale: true
         }
     }
 })
